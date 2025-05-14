ab865028083c5dea5721ffc94640ff19399d0b71
Aellantis <Ashleyyung.1231@gmail.com>
2025-05-14 10:42:37 -0700

    Updated routes

--- grocery_app/extensions.py
+++ grocery_app/extensions.py
@@ -1,9 +1,30 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from grocery_app.config import Config
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 import os
 
-app = Flask(__name__)
-app.config.from_object(Config)
 
-db = SQLAlchemy(app)
+db = SQLAlchemy()
+bcrypt = Bcrypt()
+login_manager = LoginManager()
+
+# Set login view
+login_manager.login_view = 'auth.login'
+
+def init_app(app):
+    """Initialize Flask extensions with the app."""
+    # Initialize SQLAlchemy
+    db.init_app(app)
+    
+    # Initialize Flask-Bcrypt
+    bcrypt.init_app(app)
+    
+    # Initialize Flask-Login
+    login_manager.init_app(app)
+
+    from grocery_app.models import User
+    
+    @login_manager.user_loader
+    def load_user(user_id):
+        return User.query.get(int(user_id))
--- grocery_app/routes.py
+++ grocery_app/routes.py
@@ -1,12 +1,13 @@
 from flask import Blueprint, request, render_template, redirect, url_for, flash
 from datetime import date, datetime
-from grocery_app.models import GroceryStore, GroceryItem, ItemCategory
-from grocery_app.forms import GroceryStoreForm, GroceryItemForm
+from grocery_app.models import GroceryStore, GroceryItem, User
+from grocery_app.forms import GroceryStoreForm, GroceryItemForm, SignUpForm, LoginForm
+from grocery_app.extensions import app, db, bcrypt
+from flask_login import login_user, logout_user, login_required, current_user
 
-# Import app and db from events_app package so that we can run app
-from grocery_app.extensions import app, db
-
+# Create blueprints
 main = Blueprint("main", __name__)
+auth = Blueprint("auth", __name__)
 
 ##########################################
 #           Routes                       #
@@ -18,65 +19,140 @@ def homepage():
     return render_template('home.html', all_stores=all_stores)
 
 @main.route('/new_store', methods=['GET', 'POST'])
+@login_required
 def new_store():
     form = GroceryStoreForm()
-
     if form.validate_on_submit():
         new_store = GroceryStore(
             title=form.title.data,
             address=form.address.data
         )
-        
         db.session.add(new_store)
         db.session.commit()
-        flash('Store created successfully!')
+        flash('New store was added successfully.')
         return redirect(url_for('main.store_detail', store_id=new_store.id))
-    
+
     return render_template('new_store.html', form=form)
 
 @main.route('/new_item', methods=['GET', 'POST'])
+@login_required
 def new_item():
-    form = GroceryItemForm()
-
-    if form.validate_on_submit():
+    item_form = GroceryItemForm()
+    
+    if item_form.validate_on_submit():
         new_item = GroceryItem(
-            name=form.name.data,
-            price=form.price.data,
-            category=form.category.data,
-            photo_url=form.photo_url.data,
-            store=form.store.data
+            name=item_form.name.data,
+            price=item_form.price.data,
+            category=item_form.category.data,
+            photo_url=item_form.photo_url.data,
+            store=item_form.store.data
         )
-
         db.session.add(new_item)
         db.session.commit()
-        flash('Item created successfully!')
-        
+        flash('New item was added successfully.')
         return redirect(url_for('main.item_detail', item_id=new_item.id))
 
-    return render_template('new_item.html', form=form)
+    return render_template('new_item.html', form=item_form)
 
 @main.route('/store/<store_id>', methods=['GET', 'POST'])
 def store_detail(store_id):
     store = GroceryStore.query.get(store_id)
-    form = GroceryStoreForm(obj=store)
-
-    if form.validate_on_submit():
-        form.populate_obj(store)
-        db.session.commit()
-        flash('Store updated successfully!')
-        return redirect(url_for('main.store_detail', store_id=store.id))
     
+    if store is None:
+        flash('Store not found.')
+        return redirect(url_for('main.homepage'))
+
+    form = GroceryStoreForm(obj=store)
+    if form.validate_on_submit():
+        store.title = form.title.data
+        store.address = form.address.data
+        db.session.commit()
+        flash('Store was updated successfully.')
+        return redirect(url_for('main.store_detail', store_id=store.id))
+
     return render_template('store_detail.html', store=store, form=form)
 
 @main.route('/item/<item_id>', methods=['GET', 'POST'])
 def item_detail(item_id):
     item = GroceryItem.query.get(item_id)
+    
+    if item is None:
+        flash('Error retrieving item.')
+        return redirect(url_for('main.homepage'))
+    
     form = GroceryItemForm(obj=item)
-
+    
     if form.validate_on_submit():
-        form.populate_obj(item)
+        item.name = form.name.data
+        item.price = form.price.data
+        item.category = form.category.data
+        item.photo_url = form.photo_url.data
+        item.store = form.store.data
         db.session.commit()
-        flash('Item updated successfully!')
+        
+        flash('Item was updated successfully.')
         return redirect(url_for('main.item_detail', item_id=item.id))
     
     return render_template('item_detail.html', item=item, form=form)
+
+@main.route('/add_to_shopping_list/<item_id>', methods=['POST'])
+@login_required
+def add_to_shopping_list(item_id):
+    item = GroceryItem.query.get(item_id)
+    if item is None:
+        flash('Item not found.')
+        return redirect(url_for('main.homepage'))
+        
+    current_user.shopping_list_users.append(item)
+    db.session.commit()
+    flash('Item successfully added to your shopping list!')
+    return redirect(url_for('main.shopping_list'))
+
+@main.route('/shopping_list')
+@login_required
+def shopping_list():
+    shopping_list = current_user.shopping_list_users
+    return render_template("shopping_list.html", shopping_list=shopping_list)
+
+@auth.route('/signup', methods=['GET', 'POST'])
+def signup():
+    if current_user.is_authenticated:
+        return redirect(url_for('main.homepage'))
+        
+    form = SignUpForm()
+    if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user = User(
+            username=form.username.data,
+            password=hashed_password
+        )
+        db.session.add(user)
+        db.session.commit()
+        flash('Account Created! You can now log in.')
+        return redirect(url_for('auth.login'))
+
+    return render_template('signup.html', form=form)
+
+@auth.route('/login', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('main.homepage'))
+        
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            login_user(user, remember=True)
+            next_page = request.args.get('next')
+            return redirect(next_page if next_page else url_for('main.homepage'))
+        else:
+            flash('Login unsuccessful. Please check username and password.')
+            
+    return render_template('login.html', form=form)
+
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash('You have been logged out.')
+    return redirect(url_for('main.homepage'))
